6b9a246c5d6f4bccf492044041960f87c93aa7ab
Joe Heck <heckj@mac.com>
2021-02-27 09:09:09 -0800

    Initial Commit

---
+++ Shared/X5336App.swift
@@ -0,0 +1,17 @@
+//
+//  X5336App.swift
+//  Shared
+//
+//  Created by Joseph Heck on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct X5336App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
