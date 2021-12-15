f949ab04bd5382dd4b1b3486165ba00884c47efc
Joe Heck <heckj@mac.com>
2021-12-15 14:12:40 -0800

    break out rendering commands into its own file

---
+++ Lindenmayer/Sources/Lindenmayer/TwoDRenderCommands.swift
@@ -0,0 +1,39 @@
+//
+//  TwoDRenderCommands.swift
+//  
+//
+//  Created by Joseph Heck on 12/15/21.
+//
+
+import Foundation
+
+// MARK: - RENDERING/REPRESENTATION -
+
+public enum TurnDirection: String {
+    case right = "-"
+    case left = "+"
+}
+
+public enum RollDirection: String {
+    case left = "\\"
+    case right = "/"
+}
+
+public enum BendDirection: String {
+    case up = "^"
+    case down = "&"
+}
+
+// NOTE(heckj): extensions can't be extended by external developers, so
+// if we find we want that, these should instead be set up as static variables
+// on a struct, and then we do slightly different case mechanisms.
+public enum RenderCommand : Equatable {
+    case bend(BendDirection, Double = 30)
+    case roll(RollDirection, Double = 30)
+    case move(Double = 1.0) // "f"
+    case draw(Double = 1.0) // "F"
+    case turn(TurnDirection, Double = 90)
+    case saveState // "["
+    case restoreState // "]"
+    case ignore
+}
