28aec40180ebfa4290c46f91da54da9a9e66e22f
Joe Heck <heckj@mac.com>
2022-01-11 11:44:29 -0800

    manual highlighting

--- Shared/LSystemModel.swift
+++ Shared/LSystemModel.swift
@@ -16,14 +16,21 @@ public class LSystemModel: ObservableObject {
     let renderer = SceneKitRenderer()
     let _baseSystem = Detailed3DExamples.sympodialTree
 
+    var _scene: SCNScene
+    var _transformSequence: [matrix_float4x4]
+    
     public var objectWillChange = Combine.ObservableObjectPublisher()
 
     public var scene: SCNScene {
-        renderer.generateScene(lsystem: system).0
+        get {
+            _scene
+        }
     }
 
     public var transformSequence: [matrix_float4x4] {
-        renderer.generateScene(lsystem: system).1
+        get {
+            _transformSequence
+        }
     }
 
     var _iterations = 1
@@ -36,10 +43,12 @@ public class LSystemModel: ObservableObject {
             _iterations = newValue
             objectWillChange.send()
             system = _baseSystem.evolved(iterations: _iterations)
+            (_scene, _transformSequence) = renderer.generateScene(lsystem: system)
         }
     }
 
     public init() {
         system = _baseSystem
+        (_scene, _transformSequence) = renderer.generateScene(lsystem: _baseSystem)
     }
 }
